43e630671685e5641010b13aceb5e515a71c4350
BuildTools <steve4744@gmail.com>
2020-11-22 22:17:33 +0000

    update method name to reflect change in Parkour code

--- src/io/github/steve4744/parkourtopten/ParkourTopTenCommand.java
+++ src/io/github/steve4744/parkourtopten/ParkourTopTenCommand.java
@@ -54,7 +54,7 @@ public class ParkourTopTenCommand implements CommandExecutor {
 				player.sendMessage(ChatColor.RED + "You must specify a Parkour course to display");
 				return true;
 			}
-			if (!Parkour.getInstance().getCourseManager().courseExists(arg3[1])) {
+			if (!Parkour.getInstance().getCourseManager().doesCourseExists(arg3[1])) {
 				player.sendMessage(ChatColor.RED + "Parkour course " + ChatColor.AQUA + arg3[1] + ChatColor.RED + " does not exist");
 				return true;
 			}
